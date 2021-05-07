deca5fdece9fd632027318188728b4b8076cb7c1
my_z <menzhang1@student.unimelb.edu.au>
2021-05-07 20:31:17 +1000

    test

--- client/src/commons/axios.js
+++ client/src/commons/axios.js
@@ -2,7 +2,7 @@ import _axios from 'axios';
 
 const axios = baseUrl => {
     const instance = _axios.create({
-        baseURL: 'https://info30005dev.herokuapp.com' || 'http://localhost:5000'
+        baseURL: 'https://le-sillage.herokuapp.com' || 'http://localhost:5000'
     });
     return instance;
 };
--- server.js
+++ server.js
@@ -1,39 +1,39 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
-
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 5000;
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const path = require('path');
 
 const vendor = require('./routes/vendor');
 const snack = require('./routes/snack');
 const customer = require('./routes/customer');
 const order = require('./routes/order');
 
+const port = process.env.PORT || 5000;
+
+const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 /** URL for our live website: 
  *  https://lesillage.herokuapp.com
  */
 
 //adding for texting whether the heroku is conntected 
-app.get('/', (req, res) => {
-    res.status(200)
-    res.write('<html style="text-align:center;"><h3> If you can see this message </h3></html>')
-    res.write('<html style="text-align:center;"><h3> that means you connect successfully </h3></html>')
-    res.write('<html style="text-align:center;"><h1> LE Sillage </h1></html>')
-    res.write('<html><h2> Welcome to our vendor cafe! </h2></html>')
+// app.get('/', (req, res) => {
+//     res.status(200)
+//     res.write('<html style="text-align:center;"><h3> If you can see this message </h3></html>')
+//     res.write('<html style="text-align:center;"><h3> that means you connect successfully </h3></html>')
+//     res.write('<html style="text-align:center;"><h1> LE Sillage </h1></html>')
+//     res.write('<html><h2> Welcome to our vendor cafe! </h2></html>')
 
-    res.write('<html><ul></html>')
-    res.write('<html><p font-size: 20px;> <a href="/menu">Check out our menu = )</a> </p></html>')
-    res.write('<html style="text-align:center;"><h4> Do Not click the menu, it is not work yet </h4></html>')
-    res.end('<html></ul></html>')
-})
+//     res.write('<html><ul></html>')
+//     res.write('<html><p font-size: 20px;> <a href="/menu">Check out our menu = )</a> </p></html>')
+//     res.write('<html style="text-align:center;"><h4> Do Not click the menu, it is not work yet </h4></html>')
+//     res.end('<html></ul></html>')
+// })
 
 
-
-app.use(bodyParser.json());
 const db = require('./dtbase/keys').mongoURL;
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -45,9 +45,17 @@ app.use('/snack', snack);
 app.use('/customer', customer);
 app.use('/order', order);
 
-
+// for sever static production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 // url listen..
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
+
+module.exports = app;
