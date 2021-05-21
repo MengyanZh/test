8049281f0d2e5365fa1ffd2a0b3915aa2f4f0af9
my_z <menzhang1@student.unimelb.edu.au>
2021-05-21 21:33:56 +1000

    start

--- client/src/commons/axios.js
+++ client/src/commons/axios.js
@@ -2,6 +2,8 @@ import _axios from 'axios';
 
 const axios = baseUrl => {
     const instance = _axios.create({
+
+        // 全部开发完之后再链接heroku
         baseURL: baseUrl || 'http://localhost:5000'
     });
     return instance;
